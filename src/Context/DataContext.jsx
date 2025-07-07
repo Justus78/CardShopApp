b4799d0dc1a029f719fd33c689779e4721b8bf78
Justus78 <stars787878@aol.com>
2025-07-06 21:28:11 -0400

    Added new admin components to comp folder

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,16 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getFromApi } from "../Services/LoginService";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect( () => {
+    const fetchUser = async () => {
+      const { data, error } = await getFromApi("status");
+      if(data){
+        setUser(data);
+        console.log(user);
+      }
+      else {
+        setUser(null);
+      }
+      setLoading(false);
+    };
+    fetchUser();
+  },[]);
 
   const value = {
     loading,
     setLoading,
     isAuthenticated,
     setIsAuthenticated,
+    user,
+    setUser
   };
 
   return (
