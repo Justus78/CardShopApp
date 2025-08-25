53477e17d07532f3908c29b4f96ca7466e1f6214
Justus78 <stars787878@aol.com>
2025-08-24 21:15:05 -0400

    added new pics to assets

--- src/Pages/User/Home/Home.jsx
+++ src/Pages/User/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../../Components/User/Navbar'
+import { assets } from '../../../assets/assets'
 
 const Home = () => {
   return (
@@ -8,6 +9,14 @@ const Home = () => {
       <button>
         <a href="/User/viewProducts">Products</a>
       </button>
+      <div className='border h-200'>
+        <img src={assets.EOTPic} alt="Edge of Eternities picture." className='object-fill w-full'/>
+      </div>
+      {/** below I need a div that has 5 images of the same size, these images will be associated with the most recent magic the
+       * gathering set and be linked to the product page showing cards from that set
+       * for now we will use the default image from the assets file and the images will be lined up horizontally
+       */}
+
     </>
   )
 }
---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import EOTPic from './EOTPic.jpg'
+import PlaceholderImage from './PlaceholderImage.jpg'
+
+export const assets = {
+    EOTPic,
+    PlaceholderImage
+}
