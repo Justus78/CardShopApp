b380cde52268cc45098baa24168c8e81c4272a58
Justus78 <stars787878@aol.com>
2025-09-05 19:17:54 -0400

    added new background images to assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,14 @@ import EOTPic from './EOTPic.jpg'
 import PlaceholderImage from './PlaceholderImage.jpg'
 import image1 from './image1.jpg'
 import image3 from './image3.jpg'
+import GalaxyBackground from './GalaxyBackground.png'
+import ScrollBackground from './ScrollBackground.png'
 
 export const assets = {
     EOTPic,
     PlaceholderImage,
     image1,
-    image3
+    image3,
+    GalaxyBackground,
+    ScrollBackground
 }
